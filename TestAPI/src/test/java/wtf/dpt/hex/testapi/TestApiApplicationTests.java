74c5ab2f67532b254810e2438f65d87c63cc38c2
Michal Marusinec <michal.marusinec@brainit.sk>
2022-08-09 10:28:08 +0200

    Initializing modules

---
+++ TestAPI/src/test/java/wtf/dpt/hex/testapi/TestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package wtf.dpt.hex.testapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
